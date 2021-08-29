baf650543bcf2ce3bf4bb11249d161fd53e0f19c
Nadson Barros <43866766+nadsonbarros@users.noreply.github.com>
2021-08-29 00:47:31 -0300

    Update EstadoFechado.java
    Implementação dos métodos na classe EstadoFechado

--- atividade5/src/main/java/atividade5/questao3/fase1refatoracao/EstadoFechado.java
+++ atividade5/src/main/java/atividade5/questao3/fase1refatoracao/EstadoFechado.java
@@ -5,31 +5,35 @@ import atividade5.questao3.fase0procedural.MudancaDeEstadoNaoMapeadaException;
 public class EstadoFechado implements EstadoDoProcesso {
     @Override
     public void validarProcesso() throws MudancaDeEstadoNaoMapeadaException {
-
+        System.out.println("Estado antigo: " + this.processo.getEstado());
+        this.processo.setEstado(new EstadoCriado(this.processo));
+        System.out.println("Novo estado: " + this.processo.getEstado());
     }
 
     @Override
     public void cancelarProcesso() throws MudancaDeEstadoNaoMapeadaException {
-
+        throw new MudancaDeEstadoNaoMapeadaException("Não pode cancelar processo avaliado");
     }
 
     @Override
     public void designarProcesso() throws MudancaDeEstadoNaoMapeadaException {
-
+        System.out.println("Estado antigo: " + this.estadoDoProcesso);
+        this.processo.setEstado(new EstadoCriado) = "FECHADO";
+        System.out.println("Novo estado: " + this.estadoDoProcesso);
     }
 
     @Override
     public void deferirProcesso() throws MudancaDeEstadoNaoMapeadaException {
-
+        throw new MudancaDeEstadoNaoMapeadaException("Não pode deferir processo fechado");
     }
 
     @Override
     public void indeferirProcesso() throws MudancaDeEstadoNaoMapeadaException {
-
+        throw new MudancaDeEstadoNaoMapeadaException("Não pode indeferir processo fechado");
     }
 
     @Override
     public void recorrerProcesso() throws MudancaDeEstadoNaoMapeadaException {
-
+        throw new MudancaDeEstadoNaoMapeadaException("Não pode recorrrer processo fechado");
     }
 }
